d46c5bd99a35edb48708ca257e4778218b342c0a
Cristian Ioanin <cristian.ioanin@gmail.com>
2019-02-08 21:44:45 +0200

    Add buttons for setting preferred language
    Set preferences via cookie;
    Issue: not able to set preference via express app, currently implemented stupid hybrid solution: OutOfSkillError

--- routes/landing.js
+++ routes/landing.js
@@ -1,9 +1,19 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.render('landing', { page: 'home' });
+});
+
+router.get('/pref/rou', (req, res) => {
+  res.cookie('lang_preferences', 'rolang');
+  res.redirect('back');
+});
+
+router.get('/pref/eng', (req, res) => {
+  res.cookie('lang_preferences', 'enlang');
+  res.redirect('back');
 });
 
 module.exports = router;
